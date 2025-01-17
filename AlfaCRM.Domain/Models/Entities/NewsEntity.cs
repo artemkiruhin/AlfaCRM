7250d5eb8d0a26975420791c383e0abd65a8da0f
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:25:47 +0300

    News entity

---
+++ AlfaCRM.Domain/Models/Entities/NewsEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class NewsEntity
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+    public int Watchers { get; set; }
+    
+    
+    public Guid PublisherId { get; set; }
+    public virtual EmployeeEntity Publisher { get; set; } = null!;
+
+    public virtual ICollection<DepartmentEntity> Departments { get; set; } = [];
+    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
