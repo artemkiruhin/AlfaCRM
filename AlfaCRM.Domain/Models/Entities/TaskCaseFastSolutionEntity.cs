09521a8172bb3d5fd47064726d1b334e3655ed53
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:30:16 +0300

    Case type and fast solution type entities

---
+++ AlfaCRM.Domain/Models/Entities/TaskCaseFastSolutionEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class TaskCaseFastSolutionEntity
+{
+    public Guid Id { get; set; }
+    public Guid TypeId { get; set; }
+    public virtual TaskCaseTypeEntity Type { get; set; } = null!;
+}
---
+++ AlfaCRM.Domain/Models/Entities/TaskCaseTypeEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class TaskCaseTypeEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    
+    public virtual ICollection<TaskCaseEntity> Tasks { get; set; } = [];
+    public virtual ICollection<TaskCaseFastSolutionEntity> FastSolutions { get; set; } = [];
+}
