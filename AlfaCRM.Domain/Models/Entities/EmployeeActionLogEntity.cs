1fe41394a5e82631fd551608ef328ca066405ccd
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:26:59 +0300

    Log entities and helper

---
+++ AlfaCRM.Domain/Models/Entities/EmployeeActionLogEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class EmployeeActionLogEntity
+{
+    public Guid Id { get; set; }
+    public Guid EmployeeId { get; set; }
+    public virtual EmployeeEntity Employee { get; set; } = null!;
+    public required string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ AlfaCRM.Domain/Models/Entities/LogEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class LogEntity
+{
+    public Guid Id { get; set; }
+    public required string Message { get; set; }
+    public LogTypeEntity Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
