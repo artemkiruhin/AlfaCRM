732c0cf10fce780835854a9c82637a423918d809
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:18:48 +0300

    Position entity

---
+++ AlfaCRM.Domain/Models/Entities/PositionEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class PositionEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Salary { get; set; }
+    public bool IsSalaryGross { get; set; }
+    
+    public virtual ICollection<EmployeeEntity> Employees { get; set; } = [];
+}
