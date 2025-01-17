1e1f8dbfbc9d41c9d59638176704174e79ab4d8d
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:29:26 +0300

    Task case comment entity

---
+++ AlfaCRM.Domain/Models/Entities/TaskCaseCommentEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class TaskCaseCommentEntity
+{
+    public Guid Id { get; set; }
+    public Guid PublisherId { get; set; }
+    public virtual EmployeeEntity Publisher { get; set; } = null!;
+    public Guid TaskCaseId { get; set; }
+    public virtual TaskCaseEntity TaskCase { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
