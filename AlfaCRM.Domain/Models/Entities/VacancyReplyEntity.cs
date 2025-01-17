7a87a130b1b9955c78562553dcf1945cd375f4c2
artemkiruhin <temkaunik@gmail.com>
2025-01-17 21:22:32 +0300

    Vacancy reply entity

---
+++ AlfaCRM.Domain/Models/Entities/VacancyReplyEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class VacancyReplyEntity
+{
+    public Guid Id { get; set; }
+    public required string FullName { get; set; }
+    public required string PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    public string? Description { get; set; }
+    public Guid FileId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
