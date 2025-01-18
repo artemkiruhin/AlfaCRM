389afa3c31d7c1d4f215cdecf19d298bbfa10772
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:22:46 +0300

    Not working day bid repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/INotWorkingDayBidRepository.cs
@@ -0,0 +1,14 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface INotWorkingDayBidRepository : ICrudRepository<NotWorkingDayBidEntity>
+{
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByEmployee(Guid id, CancellationToken ct);
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByEmployee(string username, CancellationToken ct);
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByType(BidTypeEntity type, CancellationToken ct);
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByPeriod(DateTime from, DateTime to, CancellationToken ct);
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByCreated(DateTime date, CancellationToken ct);
+    Task<IEnumerable<NotWorkingDayBidEntity>> GetByCreated(DateTime from, DateTime to, CancellationToken ct);
+}
