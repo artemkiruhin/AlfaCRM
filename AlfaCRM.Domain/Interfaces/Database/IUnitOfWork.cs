39d746bc52bac84399a685b1d796705456ff43a0
artemkiruhin <temkaunik@gmail.com>
2025-01-18 14:35:14 +0300

    Unit of work interface init

---
+++ AlfaCRM.Domain/Interfaces/Database/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace AlfaCRM.Domain.Interfaces.Database;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+    Task<int> SaveChangesAsync(CancellationToken ct);
+    Task<int> SaveChangesAsync();
+}
