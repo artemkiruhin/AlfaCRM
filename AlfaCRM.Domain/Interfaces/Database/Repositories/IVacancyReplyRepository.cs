449fb79ce13c1024c417b7a193fb4a801e7c934d
artemkiruhin <temkaunik@gmail.com>
2025-01-18 14:51:43 +0300

    Vacancy reply repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/IVacancyReplyRepository.cs
@@ -0,0 +1,15 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface IVacancyReplyRepository : ICrudRepository<VacancyReplyEntity>
+{
+    Task<IEnumerable<VacancyReplyEntity>> GetByName(string name, CancellationToken ct);
+    Task<IEnumerable<VacancyReplyEntity>> GetByPhone(string phoneNumber, CancellationToken ct);
+    Task<IEnumerable<VacancyReplyEntity>> GetByEmail(string email, CancellationToken ct);
+    Task<IEnumerable<VacancyReplyEntity>> GetByDescriptionContent(string descriptionContent, CancellationToken ct);
+    Task<VacancyReplyEntity?> GetByFileId(Guid id, CancellationToken ct);
+    Task<IEnumerable<VacancyReplyEntity>> GetByDate(DateTime created, CancellationToken ct);
+    Task<IEnumerable<VacancyReplyEntity>> GetByDate(DateTime from, DateTime to, CancellationToken ct);
+}
