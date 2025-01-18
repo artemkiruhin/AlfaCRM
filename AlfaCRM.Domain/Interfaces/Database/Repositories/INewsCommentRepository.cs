bc0af1f5e0922debc839ebbf2b1bb02968b69b66
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:19:47 +0300

    News comment repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/INewsCommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface INewsCommentRepository : ICrudRepository<NewsCommentEntity>
+{
+    Task<List<NewsCommentEntity>> GetByNewsId(Guid id, CancellationToken ct);
+    Task<List<NewsCommentEntity>> GetByDate(DateTime date, CancellationToken ct);
+    Task<List<NewsCommentEntity>> GetByDate(DateTime from , DateTime to, CancellationToken ct);
+}
