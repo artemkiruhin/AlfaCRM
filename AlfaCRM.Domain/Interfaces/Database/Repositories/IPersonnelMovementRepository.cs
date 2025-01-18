d8c3cdee2dd15f2157effc1da0f493034ce10af5
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:26:30 +0300

    Personnel movement repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/IPersonnelMovementRepository.cs
@@ -0,0 +1,16 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface IPersonnelMovementRepository : ICrudRepository<PersonnelMovementEntity>
+{
+    Task<IEnumerable<PersonnelMovementEntity>> GetByEmployee(Guid id);
+    Task<IEnumerable<PersonnelMovementEntity>> GetByEmployee(string username);
+    Task<IEnumerable<PersonnelMovementEntity>> GetByHR(Guid id);
+    Task<IEnumerable<PersonnelMovementEntity>> GetByHR(string username);
+    Task<IEnumerable<PersonnelMovementEntity>> GetByPeriod(DateTime from, DateTime to);
+    Task<IEnumerable<PersonnelMovementEntity>> GetHiredByDate(DateTime from, DateTime to);
+    Task<IEnumerable<PersonnelMovementEntity>> GetFiredByDate(DateTime from, DateTime to);
+    Task<IEnumerable<PersonnelMovementEntity>> GetByDate(DateTime from, DateTime to);
+}
