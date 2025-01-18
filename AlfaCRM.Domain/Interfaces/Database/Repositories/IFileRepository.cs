bb535223ee090427d1956a3df00788150f0162b9
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:05:43 +0300

    File repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/IFileRepository.cs
@@ -0,0 +1,9 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface IFileRepository : ICrudRepository<FileEntity>
+{
+    Task<IEnumerable<FileEntity>> GetByType(FileType type);
+}
