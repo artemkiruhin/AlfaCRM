9cbd745ada8c2f6d0d1f1217cd7ed98e91233918
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:17:42 +0300

    Log repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface ILogRepository : ICrudRepository<LogEntity>
+{
+    Task<IEnumerable<LogEntity>> GetByMessage(string message, CancellationToken ct);
+    Task<IEnumerable<LogEntity>> GetByType(LogTypeEntity logType, CancellationToken ct);
+    Task<IEnumerable<LogEntity>> GetByDate(DateTime date, CancellationToken ct);
+    Task<IEnumerable<LogEntity>> GetByDate(DateTime from, DateTime to, CancellationToken ct);
+}
