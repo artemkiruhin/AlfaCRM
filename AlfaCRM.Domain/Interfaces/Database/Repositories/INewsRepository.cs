1f3ee20d756db19a8043a0a256b180a5f92c9c69
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:08:40 +0300

    News repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/INewsRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface INewsRepository : ICrudRepository<NewsEntity>
+{
+    Task<IEnumerable<NewsEntity>> GetByContent(string content, CancellationToken ct);
+    Task<IEnumerable<NewsEntity>> GetByWatchers(int watchers, CancellationToken ct);
+    Task<IEnumerable<NewsEntity>> GetByWatchers(int from, int to, CancellationToken ct);
+    Task<IEnumerable<NewsEntity>> GetByCreated(DateTime date, CancellationToken ct);
+    Task<IEnumerable<NewsEntity>> GetByCreated(DateTime from, DateTime to, CancellationToken ct);
+}
