8c3f13781ef4286a05a02f15fbb7eeb5b0bc7d03
artemkiruhin <temkaunik@gmail.com>
2025-01-18 16:02:56 +0300

    Feedback repo interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/IFeedbackInterface.cs
@@ -0,0 +1,13 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface IFeedbackInterface : ICrudRepository<FeedbackEntity>
+{
+    Task<IEnumerable<FeedbackEntity>> GetByContent(string content, CancellationToken ct);
+    Task<IEnumerable<FeedbackEntity>> GetByEmail(string email, CancellationToken ct);
+    Task<IEnumerable<FeedbackEntity>> GetByType(FeedbackType type, CancellationToken ct);
+    Task<IEnumerable<FeedbackEntity>> GetByDate(DateTime date, CancellationToken ct);
+    Task<IEnumerable<FeedbackEntity>> GetByDate(DateTime from, DateTime to, CancellationToken ct);
+}
