a6e57f4364c4cf54cb66aad600f7c40fa4052d39
artemkiruhin <temkaunik@gmail.com>
2025-01-18 15:51:32 +0300

    Position repository interface

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/IPositionRepository.cs
@@ -0,0 +1,10 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface IPositionRepository : ICrudRepository<PositionEntity>
+{
+    Task<PositionEntity?> GetByName(string name);
+    Task<IEnumerable<PositionEntity>> GetBySalary(decimal salary);
+}
