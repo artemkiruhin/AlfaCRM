9647168d7515f4edba2c9244648eba5d0f5c2182
artemkiruhin <temkaunik@gmail.com>
2025-01-19 16:58:03 +0300

    Log dto

---
+++ AlfaCRM.Domain/Models/DTOs/LogDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class LogDto
+{
+    public Guid Id { get; set; }
+    public required string Message { get; set; }
+    public required string Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
