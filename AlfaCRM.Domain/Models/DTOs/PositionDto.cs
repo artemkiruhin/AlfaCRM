fb8c59b284549534f52901bf164a72cf5ed0cd26
artemkiruhin <temkaunik@gmail.com>
2025-01-19 17:10:03 +0300

    Position dto

---
+++ AlfaCRM.Domain/Models/DTOs/PositionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class PositionDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Salary { get; set; }
+    public bool IsSalaryGross { get; set; }
+}
