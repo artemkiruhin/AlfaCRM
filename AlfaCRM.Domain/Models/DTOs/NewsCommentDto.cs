0b5bd6e3032ad99a20d05daa4e795a2c9d673b2c
artemkiruhin <temkaunik@gmail.com>
2025-01-19 17:03:22 +0300

    News comment dto

---
+++ AlfaCRM.Domain/Models/DTOs/NewsCommentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class NewsCommentDto
+{
+    public Guid Id { get; set; }
+    public required string Content { get; set; }
+    public Guid EmployeeId { get; set; }
+    public Guid? RepliedCommentId { get; set; }
+    public string? RepliedCommentContent { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public required string EmployeeUsername { get; set; }
+    public required string EmployeeName { get; set; }
+    public required string EmployeeSurname { get; set; }
+    public required string EmployeePatronymic { get; set; }
+    public required string EmployeeEmail { get; set; }
+    public required string EmployeePhoneNumber { get; set; }
+    public bool EmployeeHasManagementRights { get; set; }
+}
