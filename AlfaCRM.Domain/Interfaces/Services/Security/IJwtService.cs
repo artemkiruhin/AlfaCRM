bf4576f20d6b332525ff241f3378548169a6c44f
artemkiruhin <temkaunik@gmail.com>
2025-01-20 18:59:46 +0300

    JWT service interface

---
+++ AlfaCRM.Domain/Interfaces/Services/Security/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿namespace AlfaCRM.Domain.Interfaces.Services.Security;
+
+public interface IJwtService
+{
+    string CreateToken(Guid userId);
+    bool ValidateToken(string token);
+    Guid GetUserIdFromToken(string token);
+}
