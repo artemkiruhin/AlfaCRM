d4ea1635b9588419dee418fae584d8d0658c5fc0
artemkiruhin <temkaunik@gmail.com>
2025-01-20 18:56:55 +0300

    Mapper interface created

---
+++ AlfaCRM.Domain/Interfaces/Services/Mapper/IMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace AlfaCRM.Domain.Interfaces.Services.Mapper;
+
+public interface IMapper<TEntity, TDto> where TDto : class
+{
+    TDto ToDto(TEntity entity);
+    TEntity ToEntity(TDto dto);
+}
