a1166585424963e2f0ea09ef826e64adb64a1be1
artemkiruhin <temkaunik@gmail.com>
2025-01-20 20:36:35 +0300

    Employee service interface and help methods created

---
+++ AlfaCRM.Domain/Models/Contracts/EmployeeQueryParameter.cs
@@ -0,0 +1,7 @@
+﻿namespace AlfaCRM.Domain.Models.Contracts;
+
+public record EmployeeQueryParameters
+{
+    public required EmployeeFilterContract Filter { get; init; }
+    public required EmployeeSortingParameters SortParameter { get; init; }
+}
---
+++ AlfaCRM.Domain/Models/Contracts/EmployeeQueryParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace AlfaCRM.Domain.Models.Contracts;
+
+public record EmployeeSortingParameters
+{
+    public required string PropertyName { get; init; }
+    public bool IsDescending { get; init; }
+}
