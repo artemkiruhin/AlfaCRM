d984cd7bbe9c26959f8d505a8bce853df0c8f7f5
artemkiruhin <temkaunik@gmail.com>
2025-03-14 21:30:24 +0300

    upd: base crud repo interface

--- AlfaCRM.Domain/Interfaces/Database/Repositories/Base/ICrudRepository.cs
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/Base/ICrudRepository.cs
@@ -5,10 +5,10 @@ namespace AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
 public interface ICrudRepository<TEntity> where TEntity: class
 {
     Task<IEnumerable<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity?> GetByIdAsync(Guid id);
     Task<TEntity> CreateAsync(TEntity entity);
-    Task<TEntity> UpdateAsync(TEntity entity);
-    Task<TEntity> DeleteAsync(TEntity entity);
+    TEntity Update(TEntity entity);
+    TEntity Delete(TEntity entity);
     Task<IEnumerable<TEntity>> FindRangeAsync(Expression<Func<TEntity, bool>> predicate);
     Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate);
 }
