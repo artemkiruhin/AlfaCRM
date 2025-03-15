74f65d2cb6d1d6313c34562b12e48b344b97f3b9
artemkiruhin <temkaunik@gmail.com>
2025-03-15 22:23:46 +0300

    add: result dto

---
+++ AlfaCRM.Domain/Models/DTOs/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class Result<T>
+{
+    private Result() { }
+
+    public bool IsSuccess { get; init; }
+    public string ErrorMessage { get; init; }
+    public T Data { get; init; }
+
+    public static Result<T> Failure(string errorMessage) => new() { IsSuccess = false, ErrorMessage = errorMessage };
+    public static Result<T> Success(T data) => new() { Data = data, IsSuccess = true };
+}
