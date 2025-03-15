ee3c0a67cbba126cd070a2f49f36ec78620d27b0
artemkiruhin <temkaunik@gmail.com>
2025-03-15 20:37:58 +0300

    add: hasher impl

---
+++ AlfaCRM.Services/Security/SHA256Hasher.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using AlfaCRM.Domain.Interfaces.Services.Security;
+
+namespace AlfaCRM.Services.Security;
+
+public class SHA256Hasher : IHashService
+{
+    public string ComputeHash(string message) =>
+        Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(message)));
+}
