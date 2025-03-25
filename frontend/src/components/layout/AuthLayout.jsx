b0f8d7985a1266eb1a966dacdf931bb81045391f
artemkiruhin <temkaunik@gmail.com>
2025-03-25 21:28:21 +0300

    add: layout for auth pages

---
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+import {validate} from "../../api-handlers/authHandler";
+import {useNavigate} from "react-router-dom";
+
+const AuthLayout = ({ children }) => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const validateUser = async () => {
+            const result = await validate();
+            setIsAuthenticated(result);
+            setIsLoading(false);
+        }
+
+        validateUser();
+    }, [isAuthenticated, isLoading]);
+
+    if (isLoading) {
+        return <div>Загрузка...</div>
+    }
+
+    if (!isAuthenticated) {
+        navigate("/login")
+    }
+
+    return (
+        children
+    )
+}
+
+export default AuthLayout;
