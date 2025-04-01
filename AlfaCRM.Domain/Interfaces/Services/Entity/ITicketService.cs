b46fd6b05cae932f00aaf38d636bb28cd34574f2
artemkiruhin <temkaunik@gmail.com>
2025-04-01 20:16:26 +0300

    add: service interface

---
+++ AlfaCRM.Domain/Interfaces/Services/Entity/ITicketService.cs
@@ -0,0 +1,20 @@
+﻿using AlfaCRM.Domain.Models.Contracts;
+using AlfaCRM.Domain.Models.DTOs;
+
+namespace AlfaCRM.Domain.Interfaces.Services.Entity;
+
+public interface ITicketService
+{
+    Task<Result<Guid>> Create(TicketCreateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Update(TicketUpdateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Delete(Guid id, CancellationToken ct);
+    
+    Task<Result<List<TicketShortDTO>>> GetAllShort(Guid? departmentId, Guid? senderId, CancellationToken ct);
+    Task<Result<List<TicketDetailedDTO>>> GetAll(Guid? departmentId, Guid? senderId, CancellationToken ct);
+    Task<Result<TicketDetailedDTO>> GetById(Guid id, CancellationToken ct);
+    Task<Result<TicketShortDTO>> GetByIdShort(Guid id, CancellationToken ct);
+    
+    Task<Result<Guid>> TakeToWork (Guid id, CancellationToken ct);
+    Task<Result<Guid>> Complete (Guid id, string feedback, CancellationToken ct);
+    Task<Result<Guid>> Reject (Guid id, string feedback, CancellationToken ct);
+}
