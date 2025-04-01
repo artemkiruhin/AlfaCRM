adb7c10515bb6a9954faa54fbf87e6f2dc056d00
artemkiruhin <temkaunik@gmail.com>
2025-04-01 19:56:10 +0300

    add: ticket entity

---
+++ AlfaCRM.Domain/Models/Entities/TicketEntity.cs
@@ -0,0 +1,38 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class TicketEntity
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Text { get; set; }
+    public string? Feedback { get; set; }
+    public Guid DepartmentId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public TicketStatus Status { get; set; }
+    public Guid AssigneeId { get; set; }
+
+    public virtual DepartmentEntity Department { get; set; } = null!;
+    public virtual UserEntity Assignee { get; set; } = null!;
+
+    public static TicketEntity Create(string title, string text, Guid departmentId, TicketStatus status, string? feedback, Guid assigneeId)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Title = title,
+            Text = text,
+            DepartmentId = departmentId,
+            CreatedAt = DateTime.UtcNow,
+            Status = status,
+            AssigneeId = assigneeId,
+            Feedback = status switch
+            {
+                TicketStatus.Created => null,
+                TicketStatus.InWork => null,
+                TicketStatus.Completed => feedback ?? throw new NullReferenceException("Feedback cannot be null."),
+                TicketStatus.Rejected => feedback ?? throw new NullReferenceException("Feedback cannot be null."),
+                _ => throw new ArgumentOutOfRangeException(nameof(status), status, null)
+            }
+        };
+    }
+}
---
+++ AlfaCRM.Domain/Models/Entities/TicketStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public enum TicketStatus
+{
+    Created = 0, 
+    InWork = 1,
+    Completed = 2,
+    Rejected = 3
+}
