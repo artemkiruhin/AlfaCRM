1457ffff5b64f2c0bf10ee47a0b334b91c9980ba
artemkiruhin <temkaunik@gmail.com>
2025-04-02 21:33:10 +0300

    add: validations for ticket module

--- AlfaCRM.Api/Extensions/IUserValidator.cs
+++ AlfaCRM.Api/Extensions/IUserValidator.cs
@@ -9,4 +9,6 @@ public interface IUserValidator
     Task<Result<bool>> IsAdmin(ClaimsPrincipal user, CancellationToken ct);
     Task<Result<Guid>> GetUserId(ClaimsPrincipal user, CancellationToken ct);
     Task<Result<bool>> HasRightsToWorkWithTickets(ClaimsPrincipal user, Guid? ticketAssigneeId, Guid? ticketCreatorId, CancellationToken ct);
+    Task<Result<bool>> IsAdminOrSpecDepartment(ClaimsPrincipal user, CancellationToken ct);
+    Task<Result<bool>> IsAdminOrSender(ClaimsPrincipal user, Guid ticketId, CancellationToken ct);
 }
--- AlfaCRM.Api/Extensions/UserValidator.cs
+++ AlfaCRM.Api/Extensions/UserValidator.cs
@@ -7,10 +7,12 @@ namespace AlfaCRM.Api.Extensions;
 public class UserValidator : IUserValidator
 {
     private readonly IUserService _userService;
+    private readonly ITicketService _ticketService;
 
-    public UserValidator(IUserService userService)
+    public UserValidator(IUserService userService, ITicketService ticketService)
     {
         _userService = userService;
+        _ticketService = ticketService;
     }
     public async Task<Result<bool>> IsAdminOrPublisher(ClaimsPrincipal user, CancellationToken ct)
     {
@@ -94,6 +96,60 @@ public class UserValidator : IUserValidator
         catch (Exception ex)
         {
             return Result<bool>.Failure(ex.Message);
+        }
+    }
+
+    public async Task<Result<bool>> IsAdminOrSpecDepartment(ClaimsPrincipal user, CancellationToken ct)
+    {
+        try
+        {
+            var idClaimString = user.FindFirst(ClaimTypes.NameIdentifier).Value 
+                                ?? throw new NullReferenceException("Claim id not found");
+            var id = Guid.Parse(idClaimString);
+            
+            var userModelResult = await _userService.GetById(id, ct);
+            if (!userModelResult.IsSuccess) return Result<bool>.Failure($"Can't find user with id: {id} | {userModelResult.ErrorMessage}");
+            
+            var userData = userModelResult.Data;
+            
+            var isAdmin = userData.IsAdmin;
+            var hasSpecDepartment = userData.Department is { IsSpecific: true };
+            
+            return isAdmin || hasSpecDepartment ? Result<bool>.Success(true) : Result<bool>.Failure("User cannot have spec department or admin rights");
+        }
+        catch (Exception e)
+        {
+            return Result<bool>.Failure(e.Message);
+        }
+    }
+
+    public async Task<Result<bool>> IsAdminOrSender(ClaimsPrincipal user, Guid ticketId, CancellationToken ct)
+    {
+        try
+        {
+            var idClaimString = user.FindFirst(ClaimTypes.NameIdentifier).Value 
+                                ?? throw new NullReferenceException("Claim id not found");
+            var id = Guid.Parse(idClaimString);
+            
+            var userModelResult = await _userService.GetById(id, ct);
+            if (!userModelResult.IsSuccess) return Result<bool>.Failure($"Can't find user with id: {id} | {userModelResult.ErrorMessage}");
+            
+            var userData = userModelResult.Data;
+            
+            var isAdmin = userData.IsAdmin;
+           
+            
+            var ticket = await _ticketService.GetById(ticketId, ct);
+            if (!ticket.IsSuccess) return Result<bool>.Failure($"Can't find ticket with id: {ticketId}");
+            
+            var isSender = userData.Id == ticket.Data.Creator.Id;
+            
+            
+            return isAdmin || isSender ? Result<bool>.Success(true) : Result<bool>.Failure("User is not sender or hasn't admin rights");
+        }
+        catch (Exception e)
+        {
+            return Result<bool>.Failure(e.Message);
         }
     }
 }
