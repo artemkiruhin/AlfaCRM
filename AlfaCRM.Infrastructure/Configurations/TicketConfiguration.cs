3cd9fb776ab2d4ba980e43344466d3d4bbdac939
artemkiruhin <temkaunik@gmail.com>
2025-04-02 21:34:23 +0300

    upd: separate ticket config to .cs file

---
+++ AlfaCRM.Infrastructure/Configurations/TicketConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using AlfaCRM.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlfaCRM.Infrastructure.Configurations;
+
+public class TicketConfiguration : IEntityTypeConfiguration<TicketEntity>
+{
+    public void Configure(EntityTypeBuilder<TicketEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnType("uuid");
+        builder.HasIndex(x => x.Id).IsUnique();
+            
+        builder.Property(x => x.Title).IsRequired();
+        builder.Property(x => x.Text).IsRequired();
+        builder.Property(x => x.CreatorId).IsRequired();
+        builder.Property(x => x.DepartmentId).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.Status).IsRequired();
+        
+        builder.HasOne(x => x.Creator)
+            .WithMany(x => x.Tickets)
+            .HasForeignKey(x => x.CreatorId)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(x => x.Department)
+            .WithMany(x => x.Tickets)
+            .HasForeignKey(x => x.DepartmentId)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(x => x.Assignee)
+            .WithMany(x => x.Tickets)
+            .HasForeignKey(x => x.AssigneeId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
