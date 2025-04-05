dfa2f4da4ff9ac3aa8e91578dd7d391917a78e13
artemkiruhin <temkaunik@gmail.com>
2025-04-05 20:35:39 +0300

    add: link to create ticket

--- frontend/src/components/ticket/CreateTicketButton.jsx
+++ frontend/src/components/ticket/CreateTicketButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const CreateTicketButton = () => {
+const CreateTicketButton = ({onCreatePageHandler}) => {
     return (
         // <Link to="/tickets/create" className="create-ticket-button">
         //     Создать заявку
         // </Link>
-    <div className="create-ticket-button">
+    <div className="create-ticket-button" onClick={() => onCreatePageHandler()}>
         Создать заявку
     </div>
     );
