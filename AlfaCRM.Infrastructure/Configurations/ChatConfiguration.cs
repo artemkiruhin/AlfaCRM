15a42813a32349466c4148ed9994bb5feece0dcc
artemkiruhin <temkaunik@gmail.com>
2025-04-13 21:08:15 +0300

    add: configurations

---
+++ AlfaCRM.Infrastructure/Configurations/ChatConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AlfaCRM.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlfaCRM.Infrastructure.Configurations;
+
+public class ChatConfiguration : IEntityTypeConfiguration<ChatEntity>
+{
+    public void Configure(EntityTypeBuilder<ChatEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnType("uuid");
+        builder.HasIndex(x => x.Id).IsUnique();
+        
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.AdminId).IsRequired();
+
+        builder.HasOne(x => x.Admin)
+            .WithMany(x => x.Chats)
+            .HasForeignKey(x => x.AdminId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(x => x.Messages)
+            .WithOne(x => x.Chat)
+            .HasForeignKey(x => x.ChatId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(x => x.Members).WithMany(x => x.Chats);
+    }
+}
---
+++ AlfaCRM.Infrastructure/Configurations/MessageConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using AlfaCRM.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlfaCRM.Infrastructure.Configurations;
+
+public class MessageConfiguration : IEntityTypeConfiguration<MessageEntity>
+{
+    public void Configure(EntityTypeBuilder<MessageEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnType("uuid");
+        builder.HasIndex(x => x.Id).IsUnique();
+        
+        builder.Property(x => x.Content).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.IsDeleted).IsRequired();
+        builder.Property(x => x.IsPinned).IsRequired();
+        builder.Property(x => x.SenderId).IsRequired();
+        builder.Property(x => x.ChatId).IsRequired();
+
+        builder.HasOne(x => x.Sender)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.SenderId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.Chat)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.ChatId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.RepliedMessage)
+            .WithMany(x => x.Replies)
+            .HasForeignKey(x => x.RepliedMessageId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(x => x.Replies)
+            .WithOne(x => x.RepliedMessage)
+            .HasForeignKey(x => x.RepliedMessageId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
