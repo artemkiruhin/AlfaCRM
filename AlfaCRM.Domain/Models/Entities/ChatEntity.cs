04a56850be11571a5f042346a5e1d3829de9cdd5
artemkiruhin <temkaunik@gmail.com>
2025-04-13 19:27:24 +0300

    add: entities

---
+++ AlfaCRM.Domain/Models/Entities/ChatEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class ChatEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid? AdminId { get; set; }
+    public virtual UserEntity? Admin { get; set; }
+    public virtual ICollection<MessageEntity> Messages { get; set; } = [];
+    public virtual ICollection<UserEntity> Members { get; set; } = [];
+
+    public static ChatEntity Create(string name, Guid? adminId)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+            AdminId = adminId,
+            Name = name
+        };
+    }
+}
---
+++ AlfaCRM.Domain/Models/Entities/MessageEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class MessageEntity
+{
+    public Guid Id { get; set; }
+    public required string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public bool IsPinned { get; set; }
+    public Guid SenderId { get; set; }
+    public Guid ChatId { get; set; }
+    public Guid? RepliedMessageId { get; set; }
+    public virtual UserEntity Sender { get; set; } = null!;
+    public virtual ChatEntity Chat { get; set; } = null!;
+    public virtual MessageEntity? RepliedMessage { get; set; } = null!;
+
+    public static MessageEntity Create(string content, Guid senderId, Guid? repliedMessageId, Guid chatId)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Content = content,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = null,
+            DeletedAt = null,
+            SenderId = senderId,
+            ChatId = chatId,
+            RepliedMessageId = repliedMessageId,
+            IsDeleted = false,
+            IsPinned = false
+        };
+    }
+}
