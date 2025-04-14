0681177e630dedcca051d3f3faac6e2481978428
artemkiruhin <temkaunik@gmail.com>
2025-04-14 18:56:15 +0300

    add: service interfaces

---
+++ AlfaCRM.Domain/Interfaces/Services/Entity/IChatService.cs
@@ -0,0 +1,19 @@
+﻿using AlfaCRM.Domain.Models.Contracts;
+using AlfaCRM.Domain.Models.DTOs;
+
+namespace AlfaCRM.Domain.Interfaces.Services.Entity;
+
+public interface IChatService
+{
+    Task<Result<Guid>> Create(ChatCreateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Update(ChatUpdateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Delete(Guid id, CancellationToken ct);
+    
+    Task<List<ChatShortDTO>> GetByNameAsync(string name, CancellationToken ct);
+    Task<List<ChatShortDTO>> GetByUserAsync(Guid userId, CancellationToken ct);
+    
+    Task<Result<List<ChatShortDTO>>> GetAllShort(CancellationToken ct);
+    Task<Result<List<ChatDetailedDTO>>> GetAll(CancellationToken ct);
+    Task<Result<ChatDetailedDTO>> GetById(Guid id, CancellationToken ct);
+    Task<Result<ChatShortDTO>> GetByIdShort(Guid id, CancellationToken ct);
+}
---
+++ AlfaCRM.Domain/Interfaces/Services/Entity/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using AlfaCRM.Domain.Models.Contracts;
+using AlfaCRM.Domain.Models.DTOs;
+
+namespace AlfaCRM.Domain.Interfaces.Services.Entity;
+
+public interface IMessageService
+{
+    Task<Result<Guid>> Create(MessageCreateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Update(MessageUpdateRequest request, CancellationToken ct);
+    Task<Result<Guid>> Delete(Guid id, CancellationToken ct);
+    
+    Task<Result<List<MessageDTO>>> GetAll(Guid chatId, CancellationToken ct);
+    Task<Result<MessageDTO>> GetById(Guid id, CancellationToken ct);
+}
