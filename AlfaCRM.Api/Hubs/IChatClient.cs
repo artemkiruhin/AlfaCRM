7438f565c072610bcaeb8331dcda1140b3bee069
artemkiruhin <temkaunik@gmail.com>
2025-04-18 22:46:51 +0300

    add: IChatClient

---
+++ AlfaCRM.Api/Hubs/IChatClient.cs
@@ -0,0 +1,8 @@
+﻿namespace AlfaCRM.Api.Hubs;
+
+public interface IChatClient
+{
+    Task ReceiveMessage(string username, string message);
+    Task UserJoined(string username);
+    Task UserLeft(string username);
+}
