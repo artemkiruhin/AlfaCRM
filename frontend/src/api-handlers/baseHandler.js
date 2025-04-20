27047e7032405d078250717c1de89cb68c7c9473
artemkiruhin <temkaunik@gmail.com>
2025-04-20 21:18:38 +0300

    add: set chat url

--- frontend/src/api-handlers/baseHandler.js
+++ frontend/src/api-handlers/baseHandler.js
@@ -1,7 +1,9 @@
 const SERVER_URL = 'http://localhost:5023';
 const API_URL = SERVER_URL + '/api';
+const CHAT_URL = SERVER_URL + '/chatHub';
 
 export {
     SERVER_URL,
     API_URL,
+    CHAT_URL
 }
