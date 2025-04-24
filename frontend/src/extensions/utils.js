2067d35a0ab91f8cff97274675ac117081db5139
artemkiruhin <temkaunik@gmail.com>
2025-04-24 21:15:39 +0300

    add: formatter to date-input elements

--- frontend/src/extensions/utils.js
+++ frontend/src/extensions/utils.js
@@ -18,6 +18,15 @@ const formatDate = (dateString) => {
     return new Date(dateString).toLocaleDateString('ru-RU', options);
 };
 
+const setDateToInputFormat = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    const timezoneOffset = date.getTimezoneOffset() * 60000;
+    const adjustedDate = new Date(date.getTime() - timezoneOffset);
+    return adjustedDate.toISOString().split('T')[0];
+}
+
 export {
     formatDate,
+    setDateToInputFormat
 }
