7b87a75ccbb34af6cfc5042fee18be71b57d653e
artemkiruhin <temkaunik@gmail.com>
2025-05-02 15:31:39 +0300

    add: _

---
+++ AlfaCRM.Domain/Models/DTOs/ReportTable.cs
@@ -0,0 +1,8 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class ReportTable
+{
+    public string TableName { get; set; }
+    public List<ReportColumn> Columns { get; set; } = new List<ReportColumn>();
+    public List<List<object>> Rows { get; set; } = new List<List<object>>();
+}
