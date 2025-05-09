1aed5a230d62e0701ff0e7b486252ace2763beab
artemkiruhin <temkaunik@gmail.com>
2025-05-09 21:45:07 +0300

    add: log entity

---
+++ AlfaCRM.Domain/Models/Entities/LogEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace AlfaCRM.Domain.Models.Entities;
+
+public class LogEntity
+{
+    public Guid Id { get; set; }
+    public string Message { get; set; }
+    public LogType Type { get; set; }
+    public Guid? UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public virtual UserEntity User { get; set; } = null!;
+
+    public static LogEntity Create(LogType logType, string message, Guid? userId)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Type = logType,
+            Message = message,
+            UserId = userId,
+            CreatedAt = DateTime.UtcNow
+        };
+    }
+}
