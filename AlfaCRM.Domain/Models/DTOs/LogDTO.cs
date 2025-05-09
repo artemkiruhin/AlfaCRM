a69977a23a82108b5707e49569ab1230e50c0c4f
artemkiruhin <temkaunik@gmail.com>
2025-05-09 21:45:13 +0300

    add: log dto

---
+++ AlfaCRM.Domain/Models/DTOs/LogDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public record LogDTO
+(
+    Guid Id,
+    string Message,
+    string Type,
+    string UserIdString,
+    string Username,
+    DateTime CreatedAt 
+);
