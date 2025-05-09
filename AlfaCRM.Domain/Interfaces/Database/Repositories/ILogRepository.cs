2149ef0a116d0d9cacb8f14e20d550f9ca1d6d62
artemkiruhin <temkaunik@gmail.com>
2025-05-09 21:45:37 +0300

    add: log repo

---
+++ AlfaCRM.Domain/Interfaces/Database/Repositories/ILogRepository.cs
@@ -0,0 +1,10 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories.Base;
+using AlfaCRM.Domain.Models.Entities;
+
+namespace AlfaCRM.Domain.Interfaces.Database.Repositories;
+
+public interface ILogRepository : ICrudRepository<LogEntity>
+{
+    Task<IEnumerable<LogEntity>> GetAllByUserIdAsync(Guid? userId);
+    Task<IEnumerable<LogEntity>> GetAllByTypeAsync(LogType type);
+}
---
+++ AlfaCRM.Infrastructure/Repositories/LogRepository.cs
@@ -0,0 +1,21 @@
+﻿using AlfaCRM.Domain.Interfaces.Database.Repositories;
+using AlfaCRM.Domain.Models.Entities;
+using AlfaCRM.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlfaCRM.Infrastructure.Repositories;
+
+public class LogRepository(AppDbContext context) : BaseRepository<LogEntity>(context), ILogRepository
+{
+    public async Task<IEnumerable<LogEntity>> GetAllByUserIdAsync(Guid? userId)
+    {
+        return userId.HasValue && userId != null
+            ? await DbSet.AsNoTracking().Where(log => log.UserId.Value == userId.Value).ToListAsync()
+            : await DbSet.AsNoTracking().Where(log => !log.UserId.HasValue).ToListAsync();
+    }
+
+    public async Task<IEnumerable<LogEntity>> GetAllByTypeAsync(LogType type)
+    {
+        return await DbSet.AsNoTracking().Where(log => log.Type == type).ToListAsync();
+    }
+}
