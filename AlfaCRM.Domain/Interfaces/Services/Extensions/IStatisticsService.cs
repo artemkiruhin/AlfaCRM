3a4e4b001deb5a4159d8c2f41400787abd179043
artemkiruhin <temkaunik@gmail.com>
2025-05-12 22:58:16 +0300

    add: stats service

---
+++ AlfaCRM.Domain/Interfaces/Services/Extensions/IStatisticsService.cs
@@ -0,0 +1,10 @@
+﻿using AlfaCRM.Domain.Models.DTOs;
+
+namespace AlfaCRM.Domain.Interfaces.Services.Extensions;
+
+public interface IStatisticsService
+{
+   Task<Result<IEnumerable<UsersPerDepartmentByTicketsDTO>>> GetUsersByTicketBusiness(CancellationToken ct);
+   Task<Result<bool>> DistributeTicketsByUsers(CancellationToken ct);
+   Task<Result<bool>> DistributeTicketToUser(Guid userId, Guid ticketId, CancellationToken ct);
+}
