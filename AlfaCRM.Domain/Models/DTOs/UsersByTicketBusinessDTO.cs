82c6c145fbdce9a6a8f5939920d4c498f5be411b
artemkiruhin <temkaunik@gmail.com>
2025-05-12 22:58:09 +0300

    add: dtos for distribution

---
+++ AlfaCRM.Domain/Models/DTOs/UsersByTicketBusinessDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class UsersByTicketBusinessDTO
+{
+    public Guid UserId { get; set; }
+    public string Username { get; set; }
+    public string Fullname { get; set; }
+    public int TicketsCount { get; set; }
+    public int SuggestionsCount { get; set; }
+}
---
+++ AlfaCRM.Domain/Models/DTOs/UsersPerDepartmentByTicketsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AlfaCRM.Domain.Models.DTOs;
+
+public class UsersPerDepartmentByTicketsDTO
+{
+    public Guid DepartmentId { get; set; }
+    public string DepartmentName { get; set; }
+    public List<UsersByTicketBusinessDTO> Users { get; set; }
+}
