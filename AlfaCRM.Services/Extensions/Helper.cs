e428b0ac81d45bb1fc6d2e201572276601e0d6fe
artemkiruhin <temkaunik@gmail.com>
2025-05-12 22:57:44 +0300

    add: helper

---
+++ AlfaCRM.Services/Extensions/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace AlfaCRM.Services.Extensions;
+
+public class Helper
+{
+    public static IEnumerable<T> Shuffle<T>(IEnumerable<T> source)
+    {
+        var rng = new Random();
+        var elements = source.ToList();
+
+        for (int i = elements.Count - 1; i > 0; i--)
+        {
+            int swapIndex = rng.Next(i + 1);
+            (elements[i], elements[swapIndex]) = (elements[swapIndex], elements[i]);
+        }
+
+        return elements;
+    }
+
+}
