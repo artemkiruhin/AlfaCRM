eab53bee794186517d1c76510dc1f08305e0e069
artemkiruhin <temkaunik@gmail.com>
2025-05-19 21:50:32 +0300

    upd: _

--- AlfaCRM.Api/Hubs/IChatClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AlfaCRM.Api.Hubs;
-
-public interface IChatClient
-{
-    Task ReceiveMessage(string username, string message);
-    Task UserJoined(string username);
-    Task UserLeft(string username);
-}
